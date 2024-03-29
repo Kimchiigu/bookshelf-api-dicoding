e382133dbb65aa97923e5d3795330f1d530e17fc
Kimchiigu <christopher.hygunawan@gmail.com>
2024-03-29 09:41:16 +0700

    finishing the routes and handlers

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,31 @@
-const routes = [
+const { addBookHandler, displayBookHandler, detailBookHandler, updateBookHandler, deleteBookHandler } = require("./handlers")
 
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: displayBookHandler
+    },
+    {
+        method: 'GET',
+        path: '/books/{id}',
+        handler: detailBookHandler
+    },
+    {
+        method: 'PUT',
+        path: '/books/{id}',
+        handler: updateBookHandler
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: deleteBookHandler
+    }
 ]
 
 module.exports = routes
